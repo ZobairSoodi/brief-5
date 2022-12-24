5194f943004385d7ba4ec4ca3b2c884372a736ae
ZobairSoodi <soodi.zobair.solicode@gmail.com>
2022-12-24 18:24:45 +0000

    done

--- auth-to-do/app/Http/Controllers/AuthController.php
+++ auth-to-do/app/Http/Controllers/AuthController.php
@@ -2,21 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
 
 class AuthController extends Controller
 {
-    public function getView(){
+    public function getView()
+    {
         return view("login");
     }
 
-    public function googleRedirect(){
+    public function googleRedirect()
+    {
         return Socialite::driver('google')->redirect();
     }
 
-    public function googleCallback(){
+    public function googleCallback()
+    {
         $user = Socialite::driver('google')->user();
-        dd( $user);
+        // return $user->token;
+        $google_user = User::updateOrCreate(
+            ['google_id' => $user->id],
+            [
+                'name' => $user->name,
+                'email' => $user->email,
+                'google_token' => $user->token,
+            ]
+        );
+        // return $google_user;
+        Auth::login($google_user);
+        return redirect('todos');
     }
 }
--- auth-to-do/app/Models/User.php
+++ auth-to-do/app/Models/User.php
@@ -21,6 +21,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'google_token',
+        'google_id',
     ];
 
     /**
